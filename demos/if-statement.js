782c1e6b7c6a0da55247084b172c526c5c45c172
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-10 09:18:14 -0400

    if statement

---
+++ demos/if-statement.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var number = 4;
+
+var condition = number > 5;
+
+if (condition) {
+  console.log("The number is greater than 5.");
+  console.log("hi");
+  console.log("there");
+  console.log("hi");
+  console.log("there");
+  
+} else {
+  console.log("The number is less than 5");
+  console.log("hi");
+  console.log("there");
+  console.log("hi");
+  console.log("there");
+}
+
+console.log("always happens");
+
